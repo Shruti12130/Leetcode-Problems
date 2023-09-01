02eb9e90f151547b0cc5ef9d3a3d0d287d1421ba
Shruti Jai <71483307+Shruti12130@users.noreply.github.com>
2023-09-01 14:49:14 +0530

    Time: 3 ms (89.82%), Space: 8.4 MB (42.13%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
-    int dp[100001];
-    
-    int solve(int n) {
-        if(n==0 || n==1) {
-            return n;
-        }
-        if(dp[n] != -1) {
-            return dp[n];
-        }
-        if(n%2==0) {
-            return dp[n]=solve(n/2);
-        } else {
-            return dp[n]=1+solve(n/2);
-        }
-    }
-    
     vector<int> countBits(int n) {
-        vector<int> ans(n+1);
-        memset(dp, -1, sizeof(dp));
-        for(int i=0;i<=n;i++) {
-            ans[i]=solve(i);
+        vector<int> vec(n);
+        vec.push_back(0);
+        
+        if(n==0)
+            return vec;
+        
+        for(int i=1;i<=n;i++){
+            vec[i]=vec[i/2]+i%2;
         }
-        return ans;
+        return vec;
     }
 };
