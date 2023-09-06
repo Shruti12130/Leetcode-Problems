7bfd0c6350222c31585c9fafce70a892aa6261c3
Shruti Jai <71483307+Shruti12130@users.noreply.github.com>
2023-09-06 12:51:18 +0530

    Time: 2 ms (63.15%), Space: 6.9 MB (31.09%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<string> ans;
+    int n;
+    
+    bool isValidNumber(string s) {
+        if(s.length()>1 && s[0]=='0') {
+            return 0;
+        }
+        int num=0, n=s.length(), c=0;
+        for(int i=n-1;i>=0;i--) {
+            int k=s[i]-'0';
+            num+=(k*pow(10, c));
+            c++;
+        }
+        return (num>=0 && num<=255);
+    }
+    
+    void solve(int i, int parts, string str, string &s) {
+        if(parts==4 || i==n) {
+            if(parts==4 && i==n) {
+                str.pop_back();
+                if(str.length()==n+3) {
+                    ans.push_back(str);
+                }
+            }
+            return;
+        }
+        
+        for(int c=1;c<=3 && i<n;c++) {
+            string x=s.substr(i, min(c, n-i)); 
+            if(isValidNumber(x)) {
+                solve(i+c, parts+1, str+x+".", s);
+            }
+        }
+    }
+    
+    vector<string> restoreIpAddresses(string s) {
+        n=s.length();
+        if(n>=13){
+            return ans;
+        }
+        solve(0, 0, "", s);
+        return ans;
+    }
+};
