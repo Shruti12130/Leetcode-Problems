2cd7bb5d881ac76a5b4b77739d60b368fb90bd24
Shruti Jai <71483307+Shruti12130@users.noreply.github.com>
2023-09-08 13:00:46 +0530

    Time: 4 ms (61.97%), Space: 6.1 MB (86.76%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int dp[21][21];
+    
+    bool solve(int n, int m, string &s, string &p) {
+        if(n==0 && m==0) {
+            return true;
+        }
+        if(n>0 && m<=0) {
+            char ch=p[0];
+            if(p[1]=='*'){
+                int i=n;
+                while(i>=0){
+                    if(s[i-1]==ch) {
+                        i--;
+                    } else {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+        if(n<=0 && m>0) {
+            int i=m;
+            while(i) {
+                if(p[i-1]=='*') {
+                    i-=2;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+        if(dp[n][m] != -1) {
+            return dp[n][m];
+        }
+        if(s[n-1]==p[m-1] || p[m-1]=='.') {
+            return dp[n][m] = solve(n-1, m-1, s, p);
+        }
+        if(p[m-1]=='*'){
+            if(s[n-1]!=p[m-2] && p[m-2]!='.') {
+                return dp[n][m] = solve(n, m-2, s, p);
+            }
+            return dp[n][m] = solve(n-1, m, s, p) || solve(n, m-2, s, p);
+        }
+        return dp[n][m] = false;
+    }
+    
+    bool isMatch(string s, string p) {
+        int n=s.length(), m=p.length();
+        memset(dp, -1, sizeof(dp));
+        return solve(n, m, s, p);
+    }
+};
