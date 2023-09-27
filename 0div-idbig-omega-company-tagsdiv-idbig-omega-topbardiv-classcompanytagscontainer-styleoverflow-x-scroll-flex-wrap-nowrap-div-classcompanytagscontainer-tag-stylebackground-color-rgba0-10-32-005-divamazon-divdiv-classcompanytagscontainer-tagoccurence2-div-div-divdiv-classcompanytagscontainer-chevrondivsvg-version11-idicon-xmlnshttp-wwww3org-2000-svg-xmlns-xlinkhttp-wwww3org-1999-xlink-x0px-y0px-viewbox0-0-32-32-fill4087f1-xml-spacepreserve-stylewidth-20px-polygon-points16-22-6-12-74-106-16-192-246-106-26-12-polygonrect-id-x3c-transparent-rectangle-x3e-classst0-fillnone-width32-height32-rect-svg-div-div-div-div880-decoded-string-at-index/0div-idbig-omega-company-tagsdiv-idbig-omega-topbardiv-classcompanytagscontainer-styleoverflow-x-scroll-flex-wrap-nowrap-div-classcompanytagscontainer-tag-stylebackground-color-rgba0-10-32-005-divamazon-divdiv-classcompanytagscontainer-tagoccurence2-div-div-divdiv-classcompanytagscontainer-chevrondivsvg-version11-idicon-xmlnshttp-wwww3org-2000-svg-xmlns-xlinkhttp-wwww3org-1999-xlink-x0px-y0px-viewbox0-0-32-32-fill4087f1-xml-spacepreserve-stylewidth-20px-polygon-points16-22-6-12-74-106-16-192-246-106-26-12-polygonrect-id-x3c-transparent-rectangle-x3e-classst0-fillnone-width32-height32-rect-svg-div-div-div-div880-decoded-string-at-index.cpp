7fb0c83aef68718c7c9381ba87ed742f310b35a3
Shruti Jai <71483307+Shruti12130@users.noreply.github.com>
2023-09-27 13:26:50 +0530

    Time: 2 ms (45.12%), Space: 6.4 MB (11.71%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div880-decoded-string-at-index/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div880-decoded-string-at-index.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string decodeAtIndex(string s, int k) {
+        int n=s.length(), i=0;
+        long long lengthOfDecodedString = 0;
+        while(lengthOfDecodedString < k) {
+            if(s[i]>='2' && s[i]<='9') {
+                lengthOfDecodedString *= (s[i]-'0');
+            } else {
+                lengthOfDecodedString++;
+            }
+            i++;
+        }
+        
+        for(int j=i-1;j>=0;j--) {
+            if(s[j]>='2' && s[j]<='9') {
+                lengthOfDecodedString /= (s[j]-'0');
+                k %= lengthOfDecodedString;
+            } else {
+                if(k==0 || k==lengthOfDecodedString) {
+                    return string("") + s[j];
+                }
+                lengthOfDecodedString--;
+            }
+        }
+        return "";
+    }
+};
