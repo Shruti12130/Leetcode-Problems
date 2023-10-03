aa9812bfa3d9ad3e2950af61e2023f0a674536c1
Shruti Jai <71483307+Shruti12130@users.noreply.github.com>
2023-10-03 19:57:15 +0530

    Time: 39 ms (82.24%), Space: 10.9 MB (69.91%) - LeetHub

---
+++ 0887-super-egg-drop/0887-super-egg-drop.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int dp[101][10001];
+    int solve(int egg, int floor) {
+        if(floor<=1) {
+            return floor;
+        }
+        if(egg==1) {   //Worst case scenaio if we have only 1 egg
+            return floor;
+        }
+        if(dp[egg][floor]!=-1) {
+            return dp[egg][floor];
+        }
+        int i=1, j=floor, temp=0, ans=INT_MAX;
+        while(i<=j) {
+            int mid=(i+j)/2;
+            
+            //If egg broke
+            int lo = solve(egg-1, mid-1);
+            int hi = solve(egg, floor-mid);
+            int temp=1+max(lo, hi);
+            
+            if(lo < hi) {
+                i=mid+1;
+            } else {
+                j=mid-1;
+            }
+            ans=min(ans, temp);
+        }
+        return dp[egg][floor] = ans;
+    }
+    
+    int superEggDrop(int k, int n) {
+        memset(dp, -1, sizeof(dp));
+        return solve(k, n);
+    }
+};
