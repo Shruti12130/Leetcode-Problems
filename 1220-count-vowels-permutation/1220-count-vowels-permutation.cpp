9334ccecdd9cc064bbff9aeeab0d60f36a71a122
Shruti Jai <71483307+Shruti12130@users.noreply.github.com>
2023-10-28 17:38:54 +0530

    Time: 69 ms (33.60%), Space: 24.4 MB (43.52%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -7,24 +7,19 @@ public:
         for(int i=0;i<5;i++) {
             dp[1][i]=1;
         }
-        unordered_map<int, vector<int>> mp;
-        mp[0]={1,2,4};
-        mp[1]={0,2};
-        mp[2]={1,3};
-        mp[3]={2};
-        mp[4]={2,3};
+        
+        vector<vector<int>> mp = {{1,2,4}, {0,2}, {1,3}, {2}, {2,3}};
         
         for(int i=2;i<=n;i++) {
             for(int j=0;j<5;j++) {
                 for(auto it: mp[j]){
-                    int k=it;
-                    (dp[i][j]+=dp[i-1][k]%mod)%=mod;
+                    dp[i][j] = (dp[i][j]+dp[i-1][it])%mod;
                 }
             }
         }
         for(int i=0;i<5;i++) {
-            (ans+=dp[n][i]%mod)%=mod;
+            ans=(ans+dp[n][i])%mod;
         }
-        return (int)ans%mod;
+        return (int)ans;
     }
 };
