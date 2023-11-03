91390c9241b9b4b843886b20e63bda6400eabf10
Shruti Jai <71483307+Shruti12130@users.noreply.github.com>
2023-11-03 21:30:23 +0530

    Time: 4 ms (48.55%), Space: 12.6 MB (16.61%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,22 +3,19 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n=nums.size();
         int i=n-2;
-        
-        //Find the first number that is not greater than its next adjacent number
-        while(i>=0 && nums[i] >= nums[i+1]) {
+        while(i>=0) {
+            if(nums[i]<nums[i+1]) {
+                break;
+            }
             i--;
         }
-        //if all the numbers are in decreasing order then its the largest permutation,
-        //reverse the array to get the smallest permutation for this array 
+        
         if(i<0) {
             reverse(nums.begin(), nums.end());
             return;
         }
-        
-        //Else find the first number that is greater than the above noted digit and 
-        //swap both numbers's position 
         int j=n-1;
-        while(nums[i] >= nums[j]) {
+        while(j>i && nums[i]>=nums[j]) {
             j--;
         }
         swap(nums[i], nums[j]);
