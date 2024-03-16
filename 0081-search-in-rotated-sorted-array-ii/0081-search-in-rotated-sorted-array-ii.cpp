0104cc0fdec388f17b07bfa36a50a29644f723a9
Shruti Jai <71483307+Shruti12130@users.noreply.github.com>
2024-03-16 15:45:49 +0530

    Time: 2 ms (81.37%), Space: 16.5 MB (26.93%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -13,12 +13,12 @@ public:
                 l++;
                 r--;
             } else if(nums[l] <= nums[mid]) {
-				if((nums[l] <= target) && (nums[mid] > target))
+				if((nums[l] <= target) && (nums[mid] >= target))
                     r = mid - 1;
                 else
                     l = mid + 1;
             } else {
-                if((nums[mid] < target) && (nums[r]>= target))
+                if((nums[mid] <= target) && (nums[r]>= target))
                     l = mid + 1;
                 else
                     r = mid - 1;
