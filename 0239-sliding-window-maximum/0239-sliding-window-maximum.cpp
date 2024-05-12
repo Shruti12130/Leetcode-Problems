a7a0ea20b74c7c4ea2526557e2fbc499e47ed990
Shruti Jai <71483307+Shruti12130@users.noreply.github.com>
2024-05-12 15:44:54 +0530

    Time: 156 ms (92.11%), Space: 137.2 MB (36.75%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -3,22 +3,27 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
         vector<int> ans;
-        multiset<int> ms;
-        int mx=-1e9;
+        deque<int> dq;
+        
         for(int j=0;j<n;j++) {
-            if(j<k) {
-                ms.insert(nums[j]);
-                mx=max(mx, nums[j]);
-                if(j==k-1) {
-                    ans.push_back(mx);
-                }
-                continue;
+            //To remove the left side element out of the dq which is no more a part of window
+            if(!dq.empty() && dq.front() == j-k) {
+                dq.pop_front();
             }
-            ms.insert(nums[j]);
-            ms.erase(ms.find(nums[j-k]));
-            mx=*(ms.rbegin());
-            ans.push_back(mx);
+            //To maintain the sorted order inside dq
+            //Going from left to right(front to back), elements are in sorted order
+            while(!dq.empty() && nums[dq.back()] < nums[j]) {
+                dq.pop_back();
+            }
+            
+            dq.push_back(j);
+            
+            //Pushing the element into ans vector
+            if(j>=k-1) {
+                ans.push_back(nums[dq.front()]);
+            }
         }
+        
         return ans;
     }
 };
