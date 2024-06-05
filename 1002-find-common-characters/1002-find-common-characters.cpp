73335921c3043f9a66f703a67a24a20feb82f782
Shruti Jai <71483307+Shruti12130@users.noreply.github.com>
2024-06-05 23:16:12 +0530

    Time: 3 ms (96.29%), Space: 11.5 MB (51.34%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        int n=words.size();
+        int mp[n+1][26];
+        memset(mp, 0, sizeof mp);
+        
+        vector<string> ans;
+        
+        for(int i=0;i<n;i++) {
+            string word=words[i];
+            for(auto letter: word) {
+                mp[i][letter-'a']++;
+            }
+        }
+        
+        for(int j=0;j<26;j++) {
+            int mn=1e9;
+            for(int i=0;i<n;i++) {
+                mn=min(mn, mp[i][j]);
+                if(!mp[i][j]) {
+                    break;
+                }
+            }
+            int num=mn;
+            char ch=j+'a';
+            string s="";
+            s+=ch;
+            while(num--) {
+                ans.push_back(s);
+            }
+        }
+        
+        return ans;
+    }
+};
